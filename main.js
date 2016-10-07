ec63d42745899e19f29aa9ae7fa3bccca1ba625d
Alex <quickz@inbox.lv>
2016-10-08 01:27:48 +0300

    containers to drop the elements in

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 (function($){
 
-  for (let i = 0; i < 3; i++)
-    $("#drag" + i).draggable({ containment: "#everything" });
-
-
+	for (let i = 0; i < 3; i++)
+		$("#drag" + i).draggable({
+			containment: "#everything",
+			revert: "invalid",
+			helper: "clone"
+		});
+	for (let i = 0; i < 11; i++)
+		$("#slot" + i).droppable({
+			drop: function(e, ui) {
+				ui.draggable.detach().appendTo($(this));
+			}
+		});
 
 
 
