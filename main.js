25e739974c4605d7ac52b3d823932089d8f4c4c9
Alex <quickz@inbox.lv>
2016-10-10 01:37:58 +0300

    implemented add image option

--- main.js
+++ main.js
@@ -1,21 +1,42 @@
 (function($){
 
-	for (let i = 0; i < 3; i++)
-		$("#drag" + i).draggable({
+	//for (let i = 0; i < 3; i++)
+	var images = 1;
+	imgDraggable(0);
+
+	function imgDraggable(n)
+	{
+		$("#drag" + n).draggable({
 			containment: "#everything",
 			revert: "invalid",
 			helper: "clone"
 		});
-	for (let i = 0; i < 11; i++)
+	}
+	function addImage(url)
+	{
+		$("#images").append('<img id=drag' + images + ' class="drgble" src=' + 
+			url + ' width="50px"></img>');
+		imgDraggable(images);
+		images++;
+	}
+	for (let i = 0; i < 12; i++)
 		$("#slot" + i).droppable({
 			drop: function(e, ui) {
 				$(this).empty();
-				// Moves element to container
-				ui.draggable.detach().appendTo($(this));
+				// Makes a copy of the original
+				$(this).append(ui.draggable.clone());
 			}
 		});
-
-
+	$("#test").on("click", function(){
+		$("#images").append('<img id=drag' + images + ' class="drgble"' + 
+			' src="http://i.imgur.com/y7cAttE.png" width="50px"></img>');
+		imgDraggable(images);
+		images++;
+	});
+	$("#add-img").on("click", function(){
+		addImage($("#add-txt").val());
+		$("#add-txt").val("");
+	});
 
 
 })(jQuery);
